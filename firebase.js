d7fc09e9f0b96e01363d0d42ff3c851122cdb27e
brickbooks <165932837+brickbooks@users.noreply.github.com>
2025-11-26 17:13:08 +0000

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBm7PmX1VjRYd_JHr3OPZ7RAglgD1v-rys",
+  authDomain: "brick-book-72713.firebaseapp.com",
+  projectId: "brick-book-72713",
+  storageBucket: "brick-book-72713.firebasestorage.app",
+  messagingSenderId: "970832425744",
+  appId: "1:970832425744:web:7fccf44e6f53417efd2b95"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
